9584b3e05d0baa923749cf4eb794826d427f0fac
François Bonelle <francois.bonelle@orange.com>
2016-01-06 18:55:04 +0100

    Update homedir management script

--- scripts/homedir.sh
+++ scripts/homedir.sh
@@ -1,22 +1,20 @@
+#!/bin/bash
 # This script should be placed in /etc/profile.d
-# It creates sample directories wich should be shared accross several containers
-if [ ! -e ${HOME}/.bosh ]; then
-  mkdir -p /data/.bosh
-  ln -s /data/.bosh ${HOME}/.bosh
-fi
-if [ ! -e ${HOME}/.bosh_init ]; then
-  mkdir -p /data/.bosh_init
-  ln -s /data/.bosh_init ${HOME}/.bosh_init
-fi
-if [ ! -e ${HOME}/stemcells ]; then
-  mkdir -p /data/stemcells
-  ln -s /data/stemcells ${HOME}/stemcells
-fi
-if [ ! -e ${HOME}/releases ]; then
-  mkdir -p /data/releases
-  ln -s /data/releases ${HOME}/releases
-fi
-if [ ! -e ${HOME}/shared ]; then
-  mkdir -p /data/shared
-  ln -s /data/shared ${HOME}/shared
-fi
+# It creates sample DEST_DIRectories wich should be shared accross several containers
+
+function create_dir() {
+  DIR=$1
+  DEST_DIR=/data/${DIR}
+  if [ ! -e ${HOME}/${DIR} ]; then
+    if [ ! -e ${DEST_DIR} ]; then
+      mkdir -p ${DEST_DIR}
+    fi
+    ln -s ${DEST_DIR} ${HOME}/${DIR}
+  fi
+}
+
+create_dir ".bosh"
+create_dir ".bosh_init"
+create_dir "stemcells"
+create_dir "releases"
+create_dir "shared"
