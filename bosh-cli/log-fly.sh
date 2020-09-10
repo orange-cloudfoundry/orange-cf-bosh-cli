e5e0cbd0d7f990ac93ca0f9d5747c67f425d55d7
Olivier Grand <oliv.grand@orange.com>
2020-09-10 13:28:32 +0000

    update log-fly

--- bosh-cli/log-fly.sh
+++ bosh-cli/log-fly.sh
@@ -33,27 +33,25 @@ if [ ${flagError} = 0 ] ; then
     flag=1
     printf "\n%bTeam concourse :%b\n\n" "${REVERSE}${GREEN}" "${STD}"
     printf "%b1%b  : main\n" "${GREEN}${BOLD}" "${STD}"
-    printf "%b2%b  : upload\n" "${GREEN}${BOLD}" "${STD}"
-    printf "%b3%b  : micro-depls\n" "${GREEN}${BOLD}" "${STD}"
-    printf "%b4%b  : master-depls\n" "${GREEN}${BOLD}" "${STD}"
-    printf "%b5%b  : ops-depls\n" "${GREEN}${BOLD}" "${STD}"
-    printf "%b6%b  : coab-depls\n" "${GREEN}${BOLD}" "${STD}"
-    printf "%b7%b  : remote-r2-depls\n" "${GREEN}${BOLD}" "${STD}"
-    printf "%b8%b  : remote-r3-depls\n" "${GREEN}${BOLD}" "${STD}"
-    printf "%b9%b  : cloudflare-depls\n" "${GREEN}${BOLD}" "${STD}"
-    printf "%b10%b : utils\n" "${GREEN}${BOLD}" "${STD}"
+    printf "%b2%b  : micro-depls\n" "${GREEN}${BOLD}" "${STD}"
+    printf "%b3%b  : master-depls\n" "${GREEN}${BOLD}" "${STD}"
+    printf "%b4%b  : ops-depls\n" "${GREEN}${BOLD}" "${STD}"
+    printf "%b5%b  : coab-depls\n" "${GREEN}${BOLD}" "${STD}"
+    printf "%b6%b  : remote-r2-depls\n" "${GREEN}${BOLD}" "${STD}"
+    printf "%b7%b  : remote-r3-depls\n" "${GREEN}${BOLD}" "${STD}"
+    printf "%b8%b  : cloudflare-depls\n" "${GREEN}${BOLD}" "${STD}"
+    printf "%b9%b : utils\n" "${GREEN}${BOLD}" "${STD}"
     printf "\n%bYour choice :%b " "${GREEN}${BOLD}" "${STD}" ; read choice
     case "${choice}" in
       1) TEAM="main" ;;
-      2) TEAM="upload" ;;
-      3) TEAM="micro-depls" ;;
-      4) TEAM="master-depls" ;;
-      5) TEAM="ops-depls" ;;
-      6) TEAM="coab-depls" ;;
-      7) TEAM="remote-r2-depls" ;;
-      8) TEAM="remote-r3-depls" ;;
-      9) TEAM="cloudflare-depls" ;;
-      10) TEAM="utils" ;;
+      2) TEAM="micro-depls" ;;
+      3) TEAM="master-depls" ;;
+      4) TEAM="ops-depls" ;;
+      5) TEAM="coab-depls" ;;
+      6) TEAM="remote-r2-depls" ;;
+      7) TEAM="remote-r3-depls" ;;
+      8) TEAM="cloudflare-depls" ;;
+      9) TEAM="utils" ;;
       *) flag=0 ; clear ;;
     esac
   done
