79c96d9242ba28bc0fdf2fd191218b3a12059d5b
Olivier Grand <oliv.grand@orange.com>
2021-01-05 16:59:53 +0000

    update k8s-token tool

--- bosh-cli/k8s-token.sh
+++ bosh-cli/k8s-token.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 #===========================================================================
-# show kubernetes clusters token (for k8s portals use)
+# Show kubernetes clusters token (for k8s portals use)
 #===========================================================================
 
-#--- Get k8s token
-token="$(kubectl describe secret -n kube-system $(kubectl get secret -n kube-system | grep admin | awk '{print $1}') | grep "token:" | sed -e "s+token: *++g")"
-printf "\n%bk8s token:%b\n${token}\n\n" "${YELLOW}${REVERSE}" "${STD}"
+#--- Get k8s admin token
+admin_token_name="$(kubectl -n kube-system get secret | grep admin | awk '{print $1}')"
+if [ "${admin_token_name}" = "" ] ; then
+  printf "\n%bk8s token:%b\nNo \"admin\" token available for cluster \"${K8S_CLUSTER}\".\n\n" "${YELLOW}${REVERSE}" "${STD}"
+else
+  token="$(kubectl -n kube-system describe secret ${admin_token_name} | grep "token:" | sed -e "s+token: *++g")"
+  printf "\n%bk8s token:%b\n${token}\n\n" "${YELLOW}${REVERSE}" "${STD}"
+fi
