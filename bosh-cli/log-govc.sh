97874629e18bffd181559b916109e30bb49c71ad
Olivier Grand <oliv.grand@orange.com>
2021-06-25 10:25:23 +0200

    fix log-govc

--- bosh-cli/log-govc.sh
+++ bosh-cli/log-govc.sh
@@ -25,31 +25,34 @@ if [ $? != 0 ] ; then
   fi
 fi
 
-#--- Select vcenter
-flag=0
-while [ ${flag} = 0 ] ; do
-  flag=1
-  printf "\n%bVcenter :%b\n\n" "${REVERSE}${GREEN}" "${STD}"
-  printf "%b1%b : region 1\n" "${GREEN}${BOLD}" "${STD}"
-  printf "%b2%b : region 2\n" "${GREEN}${BOLD}" "${STD}"
-  printf "\n%bYour choice :%b " "${GREEN}${BOLD}" "${STD}" ; read choice
-  case "${choice}" in
-    1) GOVC_TARGET="vcenter" ;;
-    2) GOVC_TARGET="2_vcenter" ;;
-    *) flag=0 ; clear ;;
-  esac
-done
+if [ ${flagError} = 0 ] ; then
+  #--- Select vcenter
+  flag=0
+  while [ ${flag} = 0 ] ; do
+    flag=1
+    printf "\n%bVcenter :%b\n\n" "${REVERSE}${GREEN}" "${STD}"
+    printf "%b1%b : region 1\n" "${GREEN}${BOLD}" "${STD}"
+    printf "%b2%b : region 2\n" "${GREEN}${BOLD}" "${STD}"
+    printf "\n%bYour choice :%b " "${GREEN}${BOLD}" "${STD}" ; read choice
+    case "${choice}" in
+      1) GOVC_TARGET="vcenter" ;;
+      2) GOVC_TARGET="2_vcenter" ;;
+      *) flag=0 ; clear ;;
+    esac
+  done
 
-#--- Set environment variables
-getCredhubValue "GOVC_URL" "/secrets/vsphere_${GOVC_TARGET}_ip"
-getCredhubValue "GOVC_USERNAME" "/secrets/vsphere_${GOVC_TARGET}_user"
-getCredhubValue "GOVC_PASSWORD" "/secrets/vsphere_${GOVC_TARGET}_password"
-getCredhubValue "GOVC_DATACENTER" "/secrets/vsphere_${GOVC_TARGET}_dc"
-getCredhubValue "GOVC_DATASTORE" "/secrets/vsphere_${GOVC_TARGET}_ds"
-getCredhubValue "GOVC_CLUSTER" "/secrets/vsphere_${GOVC_TARGET}_cluster"
-getCredhubValue "GOVC_RESOURCE_POOL" "/secrets/vsphere_${GOVC_TARGET}_resource_pool"
+  #--- Set environment variables
+  getCredhubValue "GOVC_URL" "/secrets/vsphere_${GOVC_TARGET}_ip"
+  getCredhubValue "GOVC_USERNAME" "/secrets/vsphere_${GOVC_TARGET}_user"
+  getCredhubValue "GOVC_PASSWORD" "/secrets/vsphere_${GOVC_TARGET}_password"
+  getCredhubValue "GOVC_DATACENTER" "/secrets/vsphere_${GOVC_TARGET}_dc"
+  getCredhubValue "GOVC_DATASTORE" "/secrets/vsphere_${GOVC_TARGET}_ds"
+  getCredhubValue "GOVC_CLUSTER" "/secrets/vsphere_${GOVC_TARGET}_cluster"
+  getCredhubValue "GOVC_RESOURCE_POOL" "/secrets/vsphere_${GOVC_TARGET}_resource_pool"
 
-export GOVC_URL GOVC_USERNAME GOVC_PASSWORD GOVC_DATACENTER GOVC_DATASTORE GOVC_CLUSTER GOVC_RESOURCE_POOL
-export GOVC_INSECURE=1 #--- vcenter region 2 is not trusted with pki
-
-printf "\n"
+  if [ ${flagError} = 0 ] ; then
+    export GOVC_URL GOVC_USERNAME GOVC_PASSWORD GOVC_DATACENTER GOVC_DATASTORE GOVC_CLUSTER GOVC_RESOURCE_POOL
+    export GOVC_INSECURE=1 #--- vcenter region 2 is not trusted with pki
+    printf "\n"
+  fi
+fi
