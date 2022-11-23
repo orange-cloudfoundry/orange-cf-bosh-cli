1e99c369066cc61aca0c5a721ab1842084618668
Olivier Grand <oliv.grand@orange.com>
2022-11-23 10:51:58 +0100

    Optimize k9s-screen tool

--- bosh-cli/k9s-screen.sh
+++ bosh-cli/k9s-screen.sh
@@ -4,17 +4,20 @@
 #===========================================================================
 
 K9S_SCREEN_DIR="/tmp/k9s-screens-bosh"
-k9sScreenShots="$(find ${K9S_SCREEN_DIR} -type f -printf "%T@ %p\n" 2> /dev/null | sort -rn | awk '{print $2}' | head -10)"
-if [ "${k9sScreenShots}" != "" ] ; then
+RES_FILE="/tmp/k9s-screens-bosh.txt"
+find ${K9S_SCREEN_DIR} -type f -printf "%T@ %p\n" 2> /dev/null | sort -rn | awk '{print strftime("%Y-%m-%d %H:%M:%S", $1) " => " $2}' | head -10 > ${RES_FILE}
+if [ ! -s ${RES_FILE} ] ; then
+  printf "\n%bNo available screenshots.%b\n" "${RED}" "${STD}"
+else
   printf "\n%bLatests screenshots :%b\n" "${REVERSE}${GREEN}" "${STD}"
-  for file in ${k9sScreenShots} ; do
-    printf "%b- ${file}\n" "${STD}"
-  done
+  latestFile="$(head -1 ${RES_FILE} | awk '{print $4}')"
+  cat ${RES_FILE} ; rm -f ${RES_FILE} > /dev/null 2>&1
+  printf "\n%bSelect a screenshot (latest by default) :%b " "${GREEN}${BOLD}" "${STD}" ; read file
+  if [ "${file}" = "" ] ; then
+    file="${latestFile}"
+  fi
 
-  printf "\n%bSelect a screenshot :%b " "${GREEN}${BOLD}" "${STD}" ; read file
   if [ -f ${file} ] ; then
     vi ${file}
   fi
-else
-  printf "\n%bNo available screenshots.%b\n" "${RED}" "${STD}"
 fi
