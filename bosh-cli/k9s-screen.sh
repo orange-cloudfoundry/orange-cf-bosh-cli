795c9c76355510e7028cb854f22c496cc9bf71c2
Olivier Grand <oliv.grand@orange.com>
2023-01-13 10:48:01 +0100

    fix issue for k9s screen

--- bosh-cli/k9s-screen.sh
+++ bosh-cli/k9s-screen.sh
@@ -5,12 +5,15 @@
 
 K9S_SCREEN_DIR="/tmp/k9s-screens-bosh"
 RES_FILE="/tmp/k9s-screens-bosh.txt"
+
+#--- Replace file name with space with dash before display
+find ${K9S_SCREEN_DIR} -type f -name "* *" | while read file ; do mv "${file}" ${file// /-} ; done
 find ${K9S_SCREEN_DIR} -type f -printf "%T@ %p\n" 2> /dev/null | sort -rn | awk '{print strftime("%Y-%m-%d %H:%M:%S", $1) " => " $2}' | head -10 > ${RES_FILE}
 if [ ! -s ${RES_FILE} ] ; then
   printf "\n%bNo available screenshots.%b\n" "${RED}" "${STD}"
 else
   printf "\n%bLatests screenshots :%b\n" "${REVERSE}${GREEN}" "${STD}"
-  latestFile="$(head -1 ${RES_FILE} | awk '{print $4}')"
+  latestFile="$(head -1 ${RES_FILE} | sed -e "s+.* => ++g")"
   cat ${RES_FILE} ; rm -f ${RES_FILE} > /dev/null 2>&1
   printf "\n%bSelect a screenshot (latest by default) :%b " "${GREEN}${BOLD}" "${STD}" ; read file
   if [ "${file}" = "" ] ; then
