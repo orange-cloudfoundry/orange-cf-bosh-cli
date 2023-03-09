10be8dfab474f1f58ea78947cea3227dc4d41495
Olivier Grand <oliv.grand@orange.com>
2023-03-09 17:43:24 +0100

    add check-k8s, init-git and kube-tail tools

---
+++ bosh-cli/check-k8s.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+#===========================================================================
+# Check k8s clusters (uses kubeconfig to select clusters to check)
+#===========================================================================
+
+#--- Load common parameters and functions
+. $(dirname $(which $0))/functions.sh
+
+#--- Check cluster resources in error
+checkClusterResources() {
+  context="$1"
+  printf "\n%b\"${context}\" suspended/failed resources...%b\n" "${REVERSE}${YELLOW}" "${STD}"
+
+  #--- Check not ready nodes
+  result="$(kubectl get nodes -A --context ${context} --request-timeout=1s --no-headers=true 2>&1)"
+  flagTimeout="$(echo "${result}" | grep "Unable to connect to the server")"
+  if [ "${flagTimeout}" != "" ] ; then
+    printf "\n%bCluster \"${context}\" not available...%b\n" "${RED}" "${STD}"
+  else
+    result="$(echo "${result}" | grep -v "Ready" | awk '{printf "%-18s %s\n", $2, $1}' | sort)"
+    if [ "${result}" != "" ] ; then
+      printf "\n%bSTATUS             NODE                                                                                        %b\n${result}\n" "${REVERSE}${GREEN}" "${STD}"
+    fi
+
+    #--- Check pods
+    result="$(kubectl get pods -A --context ${context} --no-headers=true | grep -vE "Running|Completed" | awk '{printf "%-18s %-6s %s\n", $4, $3, $1"/"$2}' | sort)"
+    if [ "${result}" != "" ] ; then
+      printf "\n%bSTATUS             READY  POD                                                                                  %b\n${result}\n" "${REVERSE}${GREEN}" "${STD}"
+    fi
+
+    #--- Check suspended/not ready flux resources (kustomization, helmchart, helmrelease, helmrepository, gitrepository)
+    result="$(flux get all -A --context ${context} | awk '{print $1 " " $2 " " $3 " " $4 " " $5}' | grep -E " False | True " | awk '{
+      if ($3 == "False" || $3 == "True") {
+        if($4 == "False" || $4 == "True") {ts=$3 ; tr=$4} else {ts="False" ; tr=$3}
+      } else {ts=$4 ; tr=$5}
+
+      if (ts == "True" || tr == "False") {
+        k=$2 ; gsub("/.*", "", k) ; n=$2 ; gsub(".*/", "", n)
+        printf "%-6s %-6s %-16s %s \n", ts, tr, k, $1"/"n
+      }
+    }')"
+
+    if [ "${result}" != "" ] ; then
+      printf "\n%bSUSP.  READY  KIND             NAMESPACE/NAME                                                                  %b\n${result}\n" "${REVERSE}${GREEN}" "${STD}"
+    fi
+  fi
+}
+
+#===================================================================================
+#--- Check k8s cluster status
+#===================================================================================
+unset https_proxy http_proxy no_proxy
+export KUBECONFIG="${HOME}/.kube/config"
+CLUSTER_CTX="$(kubectl config view -o json | jq -r ".contexts[].name")"
+printf "\n%bSelect a k8s context :%b\n${CLUSTER_CTX}" "${REVERSE}${GREEN}" "${STD}"
+printf "\n\n%bYour choice (<Enter> to select all) :%b " "${GREEN}${BOLD}" "${STD}" ; read choice
+
+if [ "${choice}" = "" ] ; then
+  for ctx in ${CLUSTER_CTX} ; do
+    checkClusterResources "${ctx}"
+  done
+else
+  checkClusterResources "${choice}"
+fi
+
+printf "\n"
---
+++ bosh-cli/init-git.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+#===========================================================================
+# Init git cli configuration and clone repositories
+#===========================================================================
+
+configureGit() {
+  flag=$(echo "${GIT_OPTIONS}" | grep " $1 ")
+  if [ "${flag}" = "" ] ; then
+    printf "\n%b- Set \"$1\" propertie...%b" "${YELLOW}" "${STD}"
+    git config --global $1 "$2"
+  fi
+}
+
+#--- Config git options
+GIT_OPTIONS=$(git config --name-only -l | sed -e "s+^+ +g" | sed -e "s+$+ +g")
+USER_NAME="$(hostname | sed -e "s+\-cli.*$++")"
+configureGit "user.name" "${USER_NAME}"
+configureGit "user.email" "${USER_NAME}@orange.com"
+
+configureGit "alias.co" "checkout"
+configureGit "alias.br" "branch"
+configureGit "alias.lol" "log --graph --decorate --pretty=oneline --abbrev-commit"
+configureGit "alias.lola" "log --graph --decorate --pretty=oneline --abbrev-commit --all"
+configureGit "alias.st" "status"
+configureGit "alias.uncommit" "reset --soft HEAD~1"
+
+configureGit "color.ui" "auto"
+
+configureGit "core.editor" "vi"
+configureGit "core.eol" "lf"
+configureGit "core.autocrlf" "input"
+configureGit "core.preloadindex" "true"
+
+configureGit "credential.helper" "cache --timeout=86400"
+configureGit "http.postbuffer" "524288000"
+configureGit "grep.linenumber" "true"
+configureGit "push.default" "tracking"
+
+#--- Display configurations
+printf "\n\n%bSet git configuration%b\n" "${REVERSE}${YELLOW}" "${STD}"
+git config -l
+
+#--- Log to credhub
+flag=$(credhub f > /dev/null 2>&1)
+if [ $? != 0 ] ; then
+  printf "\n%bLDAP user and password :%b\n" "${REVERSE}${GREEN}" "${STD}"
+  printf "username: " ; read LDAP_USER
+  credhub login --server=https://credhub.internal.paas:8844 -u ${LDAP_USER}
+  if [ $? != 0 ] ; then
+    printf "\n%bERROR : LDAP authentication failed with \"${LDAP_USER}\" account.%b\n" "${RED}" "${STD}" ; flagError=1
+  fi
+fi
+
+#--- Clone repositories
+if [ ! -d ${HOME}/bosh ] ; then
+  mkdir ${HOME}/bosh > /dev/null 2>&1
+fi
+
+cd ${HOME}/bosh
+unset http_proxy https_proxy no_proxy
+
+if [ -d ${HOME}/bosh/secrets ] ; then
+  printf "\n%bGit secrets repository already exists (delete it before if you want to reinitialize repository).%b\n" "${REVERSE}${YELLOW}" "${STD}"
+else
+  printf "\n%bClone git secrets repository%b\n" "${REVERSE}${YELLOW}" "${STD}"
+  SECRETS_URL="$(credhub g -n /secrets/concourse_git_secrets_uri -j 2> /dev/null | jq -r '.value')"
+  git clone ${SECRETS_URL} secrets 2>&1
+  if [ ! -d secrets ] ; then
+    printf "\nERROR: Git clone secrets repository failed.\n" ; exit 1
+  fi
+fi
+
+if [ -d ${HOME}/bosh/template ] ; then
+  printf "\n%bGit template repository already exists (delete it before if you want to reinitialize repository).%b\n" "${REVERSE}${YELLOW}" "${STD}"
+else
+  printf "\n%bClone git template repository%b\n" "${REVERSE}${YELLOW}" "${STD}"
+  TEMPLATE_URL="$(credhub g -n /secrets/git_template_uri -j 2> /dev/null | jq -r '.value')"
+  checkGithub="$(echo "${TEMPLATE_URL}" | grep "github")"
+  if [ "${checkGithub}" != "" ] ; then
+    export http_proxy="http://system-internet-http-proxy.internal.paas:3128"
+    export https_proxy=${http_proxy}
+    export no_proxy="127.0.0.1,localhost,169.254.0.0/16,172.17.11.0/24,192.168.0.0/16,.internal.paas,${INTRANET_DOMAINS}"
+  fi
+
+  git clone ${TEMPLATE_URL} template 2>&1
+  if [ ! -d template ] ; then
+    printf "\nERROR: Git clone template repository failed.\n" ; exit 1
+  fi
+fi
