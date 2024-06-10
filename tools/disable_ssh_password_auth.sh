0b1c478bd2e806dd29791c77e289b32f30ca48e4
Olivier Grand <oliv.grand@orange.com>
2024-06-10 11:51:51 +0200

    fix script

--- tools/disable_ssh_password_auth.sh
+++ tools/disable_ssh_password_auth.sh
@@ -6,6 +6,7 @@
 
 echo "Disabling ssh password authentication..."
 sed -i 's/^PasswordAuthentication .*/PasswordAuthentication no/g' /etc/ssh/sshd_config
-sudo sh -c "echo \"bosh:\$(date +%s | sha256sum | base64 | head -c 32 ; echo\)\" | chpasswd"
+pwd="$(echo "bosh:$(date +%s | sha256sum | base64 | head -c 32)")"
+sudo sh -c "echo ${pwd} | chpasswd"
 chage -I -1 -m 0 -M 99999 -E -1 bosh
 echo "Disabling ssh password authentication done."
