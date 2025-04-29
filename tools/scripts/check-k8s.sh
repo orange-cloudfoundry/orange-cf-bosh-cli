722a60af90682eebac76f26334e9b5b5b65adad7
Olivier Grand <oliv.grand@orange.com>
2025-04-29 08:40:16 +0200

    fix check-k8s

--- tools/scripts/check-k8s.sh
+++ tools/scripts/check-k8s.sh
@@ -17,27 +17,27 @@ checkClusterResources() {
   else
     result="$(echo "${result}" | grep -v " Ready " | awk '{print $2" "$1}' | sort)"
     if [ "${result}" != "" ] ; then
-      printf "\n%bSTATUS NODE%b\n${result}\n" "${GREEN}" "${STD}" | column -t
+      printf "\n" ; printf "%bSTATUS NODE%b\n${result}" "${GREEN}" "${STD}" | column -t
     fi
 
     #--- Check pvcs
-    result="$(kubectl get pvc -A --no-headers=true 2>&1 | grep -vE "Bound|No resources found" | awk '{print $3" "$4" "$1"/"$2}' | sort)"
+    result="$(kubectl get pvc -A --request-timeout=1s --no-headers=true 2>&1 | grep -vE "Bound|No resources found" | awk '{print $3" "$4" "$1"/"$2}' | sort)"
     if [ "${result}" != "" ] ; then
-      printf "\n%bSTATUS PVC NAMESPACE/POD%b\n${result}\n" "${GREEN}" "${STD}" | column -t
+      printf "\n" ; printf "%bSTATUS PVC NAMESPACE/POD%b\n${result}" "${GREEN}" "${STD}" | column -t
     fi
 
     #--- Check pvs
-    result="$(kubectl get pv -A --no-headers=true 2>&1 | grep -vE "Bound|No resources found" | awk '{print $5" "$1" "$6}' | sort)"
+    result="$(kubectl get pv -A --request-timeout=1s --no-headers=true 2>&1 | grep -vE "Bound|No resources found" | awk '{print $5" "$1" "$6}' | sort)"
     if [ "${result}" != "" ] ; then
-      printf "\n%bSTATUS PV NAMESPACE/POD%b\n${result}\n" "${GREEN}" "${STD}" | column -t
+      printf "\n" ; printf "%bSTATUS PV NAMESPACE/POD%b\n${result}" "${GREEN}" "${STD}" | column -t
     fi
 
     #--- Check longhorn volumes attachment
-    result="$(kubectl get volumes.longhorn.io -n 02-longhorn -o json 2>&1)"
+    result="$(kubectl get volumes.longhorn.io -n 02-longhorn -o json --request-timeout=1s 2>&1)"
     if [ $? = 0 ] ; then
-      result="$(echo "${result}" | jq -r '.items[]|.status.state + "/" + .status.robustness + " " + .metadata.name + " " + (.spec.nodeID // "" | if . == "" then "-" end) + "/" + .metadata.namespace + "/" + .status.kubernetesStatus.workloadsStatus[].podName' | grep -v "attached/healthy" | awk '{print $1" "$2" "$3" "$4}')"
+      result="$(echo "${result}" | jq -r '.items[]|.status.state + "/" + .status.robustness + " " + .metadata.name + " " + .metadata.namespace + "/" + .status.kubernetesStatus.workloadsStatus[].podName + " " + (.spec.nodeID // "" | if . == "" then "-" end)' | grep -v "attached/healthy" | awk '{print $1" "$2" "$3" "$4}')"
       if [ "${result}" != "" ] ; then
-        printf "\n%bSTATUS PVC NODE/NAMESPACE/POD%b\n${result}\n" "${GREEN}" "${STD}" | column -t
+        printf "\n" ; printf "%bSTATUS PVC NAMESPACE/POD NODE%b\n${result}" "${GREEN}" "${STD}" | column -t
       fi
     fi
 
@@ -48,12 +48,12 @@ checkClusterResources() {
     }')"
 
     if [ "${failed_suspended_resources}" != "" ] ; then
-      printf "\n%bREADY SUSP. KIND NAMESPACE/NAME%b\n${failed_suspended_resources}\n" "${GREEN}" "${STD}" | column -t
+      printf "\n" ; printf "%bREADY SUSP. KIND NAMESPACE/NAME%b\n${failed_suspended_resources}" "${GREEN}" "${STD}" | column -t
     fi
 
     #--- Check drift events on helmreleases
     result=""
-    drifted_helmReleases="$(kubectl events -A --types=Warning --no-headers=true | grep " DriftDetected *HelmRelease" | sed -e "s+.* state of release ++g" -e "s+ .*++g" -e "s+\.v.*++g" | sort)"
+    drifted_helmReleases="$(kubectl events -A --types=Warning --request-timeout=1s --no-headers=true | grep " DriftDetected *HelmRelease" | sed -e "s+.* state of release ++g" -e "s+ .*++g" -e "s+\.v.*++g" | sort)"
     if [ "${drifted_helmReleases}" != "" ] ; then
       #--- Check if HelmRelease status is failed/suspended
       for helmrelease in ${drifted_helmReleases} ; do
@@ -64,28 +64,26 @@ checkClusterResources() {
       done
 
       if [ "${result}" != "" ] ; then
-        printf "\n%bSTATUS KIND%b\n${result}\n" "${GREEN}" "${STD}" | column -t
+        printf "\n" ; printf "%bSTATUS KIND%b\n${result}" "${GREEN}" "${STD}" | column -t
       fi
     fi
 
-    #--- Check pending services
-    pending_services="$(kubectl get svc -A --context ${context} --request-timeout=1s --no-headers=true 2>&1 | grep " LoadBalancer " | grep -E "<none>|<pending>" | awk '{print $1"/"$2}')"
+    #--- Check services
+    result="$(kubectl get svc -A --context ${context} --request-timeout=1s --no-headers=true 2>&1 | grep " LoadBalancer " | grep -E "<none>|<pending>" | sed -e "s+<++g" -e "s+>++g" | awk '{print $5" "$1"/"$2}')"
     if [ "${pending_services}" != "" ] ; then
-      printf "\n%bPending services%b" "${GREEN}" "${STD}"
-      printf "\n${pending_services}\n" "${GREEN}" "${STD}" | column -t
+      printf "\n" ; printf "%bSTATUS NAMESPACE/SVC%b\n${result}" "${GREEN}" "${STD}" | column -t
     fi
 
     #--- Check pods (except vcluster ones)
-     failed_pods="$(kubectl get pods -A -o wide -l 'vcluster.loft.sh/managed-by notin (vcluster)' --context ${context} --no-headers=true | grep -vE "Completed|ContainerCreating|Terminating" | sed -e "s+(.*)++g" | awk '{ready=$3;gsub("/.*","",ready);total=$3;gsub(".*/","",total);if(ready != total){print $4" "$3" "$1"/"$2" "$8}}' | sort)"
+     failed_pods="$(kubectl get pods -A -o wide -l 'vcluster.loft.sh/managed-by notin (vcluster)' --context ${context} --request-timeout=1s --no-headers=true | grep -vE "Completed|ContainerCreating|Terminating" | sed -e "s+(.*)++g" | awk '{ready=$3;gsub("/.*","",ready);total=$3;gsub(".*/","",total);if(ready != total){print $4" "$3" "$1"/"$2" "$8}}' | sort)"
     if [ "${failed_pods}" != "" ] ; then
-      printf "\n%bSTATUS READY NAMESPACE/POD NODE%b\n${failed_pods}\n" "${GREEN}" "${STD}" | column -t
+      printf "\n" ; printf "%bSTATUS READY NAMESPACE/POD NODE%b\n${failed_pods}" "${GREEN}" "${STD}" | column -t
     fi
 
     #--- Get custom resources definitions with existing "deletionTimestamp"
-    result="$(kubectl get crds -o json 2>/dev/null | jq -r '.items[]?.metadata|select(.deletionTimestamp != null)|.name')"
+    result="$(kubectl get crds -o json --request-timeout=1s 2>/dev/null | jq -r '.items[]?.metadata|select(.deletionTimestamp != null)|.name')"
     if [ "${result}" != "" ] ; then
-      printf "\n%bCustom resources definitions with \"deletionTimestamp\"%b" "${GREEN}" "${STD}"
-      printf "\n${result}\n" "${GREEN}" "${STD}" | column -t
+      printf "\n%bCRDS with \"deletionTimestamp\"%b\n${result}\n" "${GREEN}" "${STD}"
     fi
   fi
 }
